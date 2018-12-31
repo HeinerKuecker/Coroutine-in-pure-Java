f19350c78e8e1dcf19149ae62fe99a5adca39c30
HeinerKuecker <Conrad@DESKTOP-M78SQFD>
2018-12-31 21:40:00 +0100

    check for get procedure argument not in procedure

---
+++ COROUTINE/src/de/heinerkuecker/coroutine_iterator/step/simple/NegateGlobalVar.java
@@ -0,0 +1,83 @@
+package de.heinerkuecker.coroutine_iterator.step.simple;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import de.heinerkuecker.coroutine_iterator.CoroIteratorOrProcedure;
+import de.heinerkuecker.coroutine_iterator.expression.GetProcedureArgument;
+import de.heinerkuecker.coroutine_iterator.step.CoroIterStep;
+import de.heinerkuecker.coroutine_iterator.step.result.CoroIterStepResult;
+
+public final class NegateGlobalVar<RESULT>
+extends SimpleStep<RESULT/*, CoroutineIterator<RESULT>*/>
+{
+    /**
+     * Name of variable to negate in
+     * {@link CoroIteratorOrProcedure#globalVars()}
+     */
+    public final String varName;
+
+    /**
+     * Constructor.
+     */
+    public NegateGlobalVar(
+            final String varName )
+    {
+        this.varName =
+                Objects.requireNonNull(
+                        varName );
+    }
+
+    public static <RESULT> NegateGlobalVar<RESULT> negate(
+            final String varName )
+    {
+        return new NegateGlobalVar<>(
+                varName );
+    }
+
+    /**
+     * Set variable.
+     *
+     * @see SimpleStep#execute
+     */
+    @Override
+    public CoroIterStepResult<RESULT> execute(
+            final CoroIteratorOrProcedure<RESULT> parent )
+    {
+        final Object varValue = parent.globalVars().get( varName );
+
+        if ( varValue instanceof Boolean )
+        {
+            parent.globalVars().put( varName , ! (boolean) varValue );
+        }
+        else
+            // null or not boolean is handled as false
+        {
+            parent.globalVars().put( varName , true );
+        }
+        return CoroIterStepResult.continueCoroutine();
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return varName + " = ! " + varName +
+                ( this.creationStackTraceElement != null
+                    ? " " + this.creationStackTraceElement
+                    : "" );
+    }
+
+    /**
+     * @see CoroIterStep#getProcedureArgumentsNotInProcedure()
+     */
+    @Override
+    public List<GetProcedureArgument<?>> getProcedureArgumentGetsNotInProcedure()
+    {
+        return Collections.emptyList();
+    }
+
+}
